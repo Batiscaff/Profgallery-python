a9a684901debf5e010e69e26910a5dde29d1a48b
Miroshinsv <muxa42@yandex.ru>
2016-05-30 16:09:46 +0300

    Добавлен метод vacancy_search

--- swag-python/test_vacancy_json.py
+++ swag-python/test_vacancy_json.py
@@ -47,6 +47,32 @@ class test_regUser(unittest.TestCase):
         vacId = randint(0,vacId)
         vacId = str(rest["items"][vacId]["id"])
 
+    def test_vacancy_find(self):
+        #Берем случайный ID отрасли
+        url = self.base_url + "vocabulary/17/tree/?token=" + accessToken
+        r = requests.get(url,self.head)
+        rest = json.loads(r.text)
+        indId =  rest["items"][randint(0,len(rest))]["element"]["id"]
+
+        url = self.base_url + "vacancy/find/?token=" + accessToken
+        userInfo = {
+          "industryId": indId,
+          "isDirect": "false",
+          "properties": [
+          ]
+        }
+        r = requests.post(url=url,data=json.dumps(userInfo),headers=self.head)
+        rest = json.loads(r.text)
+        #Проверка на success
+        checkStatus = rest["status"]
+        self.assertEqual(checkStatus,"success")
+
+        #Проверка на пустой массив.
+        if len(rest["items"]) == 0:
+            pass
+        #Если не пустой - сравниваем ID отрасли любой вакансии и той отрасли, что мы забивал
+        else:
+            self.assertEqual(indId,rest["items"][randint(0,len(rest))]["industry"]["id"])
 
 
 if __name__ == "__main__":
