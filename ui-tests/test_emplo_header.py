eef2eeea601d7c8905a0535c185dfe39df0bc20d
Miroshinsv <muxa42@yandex.ru>
2016-09-05 20:02:06 +0300

    Добавлен тест emplo_header

---
+++ ui-tests/test_emplo_header.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from selenium import webdriver
+
+class findEmploHeader(unittest.TestCase):
+    def setUp(self):
+        self.base_url = "http://admin:hlj1ErT@Pg.fvds.ru"
+        self.webdriver = webdriver.Firefox()
+
+    def tearDown(self):
+        self.webdriver.quit()
+
+    def test_01_emplohead(self):
+        driver = self.webdriver
+        url = self.base_url
+        driver.get(url + '/login')
+        driver.find_element_by_id('uName').send_keys("sm@profgallery.com")
+        driver.find_element_by_id('uPassword').send_keys("123456")
+        driver.find_element_by_xpath("//div[5]/div/input").click()
+        self.webdriver.find_element_by_xpath("//a[contains(text(),'Поиск сотрудников')]")
+        driver.get(url + '/employee/cabinet/profile/')
+        self.webdriver.find_element_by_xpath("//a[contains(text(),'Поиск сотрудников')]")
+        driver.get(url + '/employee/cabinet/vakansii/')
+        self.webdriver.find_element_by_xpath("//a[contains(text(),'Поиск сотрудников')]")
+        driver.get(url + '/employee/cabinet/messages/')
+        self.webdriver.find_element_by_xpath("//a[contains(text(),'Поиск сотрудников')]")
+
+
+
