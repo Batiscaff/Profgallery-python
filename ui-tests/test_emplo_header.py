25e9b51a714f0d354fd99b299a9a78d5c8f35cdf
Miroshinsv <muxa42@yandex.ru>
2016-09-06 19:09:08 +0300

    Поправлен тест vacancy_properties_add: изменен диапазон случайных значений

--- ui-tests/test_emplo_header.py
+++ ui-tests/test_emplo_header.py
@@ -1,4 +1,4 @@
-# -*- coding: Windows-1251 -*-
+# -*- coding: utf-8 -*-
 
 import unittest,sys
 reload(sys)
@@ -20,13 +20,13 @@ class findEmploHeader(unittest.TestCase):
         driver.find_element_by_id('uName').send_keys("sm@profgallery.com")
         driver.find_element_by_id('uPassword').send_keys("12378999")
         driver.find_element_by_xpath("//div[5]/div/input").click()
-        self.webdriver.find_element_by_xpath("//a[contains(text(),'Поиск сотрудников')]")
+        self.webdriver.find_element_by_xpath("//a[contains(text(),'РџРѕРёСЃРє СЃРѕС‚СЂСѓРґРЅРёРєРѕРІ')]")
         driver.get(url + '/employee/cabinet/profile/')
-        self.webdriver.find_element_by_xpath("//a[contains(text(),'Поиск сотрудников')]")
+        self.webdriver.find_element_by_xpath("//a[contains(text(),'РџРѕРёСЃРє СЃРѕС‚СЂСѓРґРЅРёРєРѕРІ')]")
         driver.get(url + '/employee/cabinet/vakansii/')
-        self.webdriver.find_element_by_xpath("//a[contains(text(),'Поиск сотрудников')]")
+        self.webdriver.find_element_by_xpath("//a[contains(text(),'РџРѕРёСЃРє СЃРѕС‚СЂСѓРґРЅРёРєРѕРІ')]")
         driver.get(url + '/employee/cabinet/messages/')
-        self.webdriver.find_element_by_xpath("//a[contains(text(),'Поиск сотрудников')]")
+        self.webdriver.find_element_by_xpath("//a[contains(text(),'РџРѕРёСЃРє СЃРѕС‚СЂСѓРґРЅРёРєРѕРІ')]")
 
 
 
