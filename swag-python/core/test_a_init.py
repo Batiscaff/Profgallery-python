94bf7dca1f0081f95b2a63c31ca052c30a594ccc
Miroshinsv <muxa42@yandex.ru>
2016-10-07 19:16:42 +0300

    тимтим

--- swag-python/core/test_a_init.py
+++ swag-python/core/test_a_init.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 import requests, json, unittest,allure
 
-@allure.MASTER_HELPER.testcase("Иницализация тестов")
+@allure.MASTER_HELPER.story("Иницализация тестов")
 class setup(unittest.TestCase):
     def setUp(self):
         self.base_url = "http://api-test.corp.profgallery.ru/api/"
         self.head = {"Content-Type": "application/json", "Accept": "application/json"}
 
-    @allure.MASTER_HELPER.step("Инициализация тестовой базы")
+    @allure.MASTER_HELPER.single_step("Инициализация тестовой базы")
     def test_01(self):
         url = self.base_url + "tests-init/"
         r = requests.get(url, self.head)
